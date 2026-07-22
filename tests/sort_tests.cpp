7e7da594ba05e55af38cadb3f782fcbae3ab3c48
HeYinKazune <1254462962@qq.com>
2026-07-22 17:05:36 +0900

    add sort test functions

---
+++ tests/sort_tests.cpp
@@ -0,0 +1,16 @@
+#include <sort.hpp>
+
+#include "sort_test_suite.hpp"
+
+REGISTER_INTEGER_SORT_TESTS(BubbleSortTest, bubble_sort)
+
+REGISTER_INTEGER_SORT_TESTS(InsertionSortTest, insertion_sort)
+
+REGISTER_INTEGER_SORT_TESTS(CockTailSortTest, cocktail_sort)
+
+REGISTER_INTEGER_SORT_TESTS(BucketSortTest, bucket_sort)
+
+REGISTER_INTEGER_SORT_TESTS(CountingSortTest, counting_sort)
+
+REGISTER_INTEGER_SORT_TESTS(MergeSortTest, merge_sort)
+
