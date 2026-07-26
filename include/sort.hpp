63108622d3109e51b5ab6465ecd2faf921c7d7ca
HeYinKazune <1254462962@qq.com>
2026-07-26 14:41:45 +0900

    add heap sort impl

--- include/sort.hpp
+++ include/sort.hpp
@@ -297,9 +297,75 @@ void shell_sort(std::vector<T>& nums) {
     }
 }
 
+//                                   0
+//                                 /   \
+// Exmaple Heap Structure:        1     2   
+//                               / \   / \
+//                              3   4 5   6
+int inline _heap_l_child_idx(int root_idx) {return 2 * root_idx + 1;} // 0 -> 1 ; 1 -> 3
+int inline _heap_r_child_idx(int root_idx) {return 2 * root_idx + 2;} // 0 -> 2 ; 1 -> 4
+int inline _heap_father_idx(int child_idx) {return (child_idx - 1) / 2;} // 1 or 2 -> 0 ; 3 or 4 -> 1
+
+template<typename T>
+void inline _make_heap_impl(std::vector<T>& nums) {
+    const int n = nums.size();
+    int heap_size = 1;
+    while (heap_size != n) {
+        auto insert_idx = heap_size;
+        while (true) {
+            auto father_idx = _heap_father_idx(insert_idx);
+            // Swap root and child
+            if (nums[insert_idx] > nums[father_idx]) std::swap(nums[insert_idx], nums[father_idx]);
+            else break;
+            // End of heap (strict-order procedure)
+            if (father_idx == 0) break;
+            // Continue search along father node
+            auto old_father = father_idx;
+            father_idx = _heap_father_idx(old_father);
+            insert_idx = old_father;
+        }
+        heap_size++;
+    }
+}
+
+template<typename T>
+void inline _heap_sort_impl(std::vector<T>& nums) {
+    int heap_size = nums.size();
+    while (heap_size > 0) {
+        // Move top element to tail
+        std::swap(nums[heap_size - 1], nums[0]);
+        heap_size--;
+        // Maintain heap from idx=0
+        int idx = 0;
+        while (idx < heap_size) {
+            auto l_child_idx = _heap_l_child_idx(idx);
+            auto r_child_idx = _heap_r_child_idx(idx);
+            // Left child index outside heap means that
+            // we do not need to compare two childs
+            // since left one always comes smaller idx.
+            if (l_child_idx >= heap_size) break;
+            // Left child is in range but right child is out of range.
+            else if (r_child_idx >= heap_size) {
+                if (nums[idx] > nums[l_child_idx]) break;
+                std::swap(nums[idx], nums[l_child_idx]);
+                idx = l_child_idx;
+            // Two childs is in range.
+            } else {
+                auto swap_child_idx = nums[l_child_idx] > nums[r_child_idx] ? l_child_idx : r_child_idx;
+                if (nums[idx] > nums[swap_child_idx]) break;
+                std::swap(nums[idx], nums[swap_child_idx]);
+                idx = swap_child_idx;
+            }
+        }
+    }
+}
+
 template<typename T>
 void heap_sort(std::vector<T>& nums) {
-    
+    const int n = nums.size();
+    if (n < 2) return;
+    _make_heap_impl(nums);
+    _heap_sort_impl(nums);
 }
 
 template<typename T>
